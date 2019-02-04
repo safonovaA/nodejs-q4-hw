3c9f21eea4b0ebd941d67af38d9c42ae87307b02
Anna Safonova <aasafonov@bk.ru>
2019-02-04 22:06:11 +0300

    Implement echo server

---
+++ src/http-servers/echo-server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const Transform = require('stream').Transform;
+
+const port = 3000;
+const server = http.createServer();
+
+const transformRequest = (req) => new Transform({
+  transform(chunk, encoding, callback) {
+    const { 
+      method,
+      url,
+      httpVersion,
+    } = req;
+    this.push(JSON.stringify({
+          requestLine: `${method} ${url} HTTP/${httpVersion}`,
+          headers: req.headers,
+          messageBody: chunk.toString(),
+        }));
+    callback();
+  }
+});
+ 
+server.on('request', (req, res) => {
+  req.pipe(transformRequest(req)).pipe(res);
+});
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`echo-server is listening on ${port}`)
+});
